21a8ca71ddc4ed4f3cb9c93995baadfd9b1f0292
Jerald-Joyson <jeraldjoyson.inc@gmail.com>
2023-02-26 01:04:21 +0530

    incomming and outgoing Activites are created and month Textfield is set to both Activities

--- app/src/main/java/com/example/fundmanagement/IncomingActivity.java
+++ app/src/main/java/com/example/fundmanagement/IncomingActivity.java
@@ -2,13 +2,101 @@ package com.example.fundmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class IncomingActivity extends AppCompatActivity {
+    final Calendar myCalendar= Calendar.getInstance();
+    TextView mnthViewInc;
+    EditText dateView,incName,incAmt;
+    Button btnIncSave;
 
+    StringBuilder ss = new StringBuilder();
+    String data,Mnth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incoming);
+        incName=(EditText) findViewById(R.id.incName);
+        incAmt=(EditText) findViewById(R.id.incAmt);
+        btnIncSave=(Button) findViewById(R.id.btnIncSave);
+
+        mnthViewInc = (TextView)findViewById(R.id.mnthViewInc);
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("MMMM");
+        Mnth= sdf.format(c.getTime());
+        mnthViewInc.setText(Mnth);
+
+        dateView=(EditText) findViewById(R.id.dateView);
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                updateLabel();
+            }
+        };
+        dateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(IncomingActivity.this,date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        btnIncSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (incName.getText().toString().equals(""))
+                {
+                    incName.setError("Plz provide Name");
+                }
+                else if(incAmt.getText().toString().equals(""))
+                {
+                    incAmt.setError("Plz provide Amount");
+                }
+                else if(dateView.getText().toString().equals(""))
+                {
+                    dateView.setError("Plz provide Date");
+                }
+                else{
+
+                    data=dateView.getText().toString();
+                    ss.append("Date:");
+                    ss.append(data);
+                    ss.append("\n");
+
+                    data=incName.getText().toString();
+                    ss.append("Name:");
+                    ss.append(data);
+                    ss.append("\n");
+
+                    data=incAmt.getText().toString();
+                    ss.append("Amount:");
+                    ss.append(data);
+                    ss.append("\n");
+
+                    Toast.makeText(getApplicationContext(),ss +"Saved successfully...!",Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
+    }
+    private void updateLabel(){
+        String myFormat="dd/MM/yy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
+        dateView.setText(dateFormat.format(myCalendar.getTime()));
     }
 }
--- app/src/main/java/com/example/fundmanagement/OutgoingActivity.java
+++ app/src/main/java/com/example/fundmanagement/OutgoingActivity.java
@@ -2,13 +2,105 @@ package com.example.fundmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class OutgoingActivity extends AppCompatActivity {
+    final Calendar myCalendar= Calendar.getInstance();
+    TextView mnthViewOut;
+    EditText dateView1,outName,outAmt;
+    Button btnOutSave;
 
+    StringBuilder ss = new StringBuilder();
+    String data,Mnth;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_outgoing);
+
+        outName = (EditText) findViewById(R.id.incName);
+        outAmt = (EditText) findViewById(R.id.incAmt);
+        btnOutSave=(Button) findViewById(R.id.btnOutSave);
+
+        mnthViewOut = (TextView)findViewById(R.id.mnthViewOut);
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("MMMM");
+        Mnth= sdf.format(c.getTime());
+        mnthViewOut.setText(Mnth);
+
+        dateView1=(EditText) findViewById(R.id.dateView1);
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                updateLabel();
+            }
+        };
+        dateView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(OutgoingActivity.this,date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        btnOutSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (dateView1.getText().toString().equals(""))
+                {
+                    dateView1.setError("Plz provide Name");
+                }
+                else if(outAmt.getText().toString().equals(""))
+                {
+                    outAmt.setError("Plz provide Amount");
+                }
+                else if(outName.getText().toString().equals(""))
+                {
+                    outName.setError("Plz provide Date");
+                }
+                else{
+
+                    data=dateView1.getText().toString();
+                    ss.append("Date:");
+                    ss.append(data);
+                    ss.append("\n");
+
+                    data=outAmt.getText().toString();
+                    ss.append("Amount:");
+                    ss.append(data);
+                    ss.append("\n");
+
+                    data=outName.getText().toString();
+                    ss.append("Amount for:");
+                    ss.append(data);
+                    ss.append("\n");
+
+                    Toast.makeText(getApplicationContext(),ss +"Registration successfully...",Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
+
+    }
+    private void updateLabel(){
+        String myFormat="dd/MM/yy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
+        dateView1.setText(dateFormat.format(myCalendar.getTime()));
     }
 }
