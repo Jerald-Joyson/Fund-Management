72cf8aa1f37452abc84bb0e5176e11d84dc6974f
Jerald-Joyson <jeraldjoyson.inc@gmail.com>
2023-02-25 09:59:37 +0530

    incoming and outgoing Pages were Created and connected to the main page

--- app/src/main/java/com/example/fundmanagement/MainActivity.java
+++ app/src/main/java/com/example/fundmanagement/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.fundmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView resAmt;
+    Button btnInc,btnOut;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resAmt=findViewById(R.id.resAmt);
+        btnInc=findViewById(R.id.btnInc);
+        btnOut=findViewById(R.id.btnOut);
+
+        btnInc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, IncomingActivity.class));
+            }
+        });
+        btnOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, OutgoingActivity.class));
+            }
+        });
+
+
     }
 }
